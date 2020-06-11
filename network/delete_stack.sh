7d8de6ad70dd452d2d810209c981358b34dab9b1
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-11 22:51:31 +0100

    Created network stack

--- network/delete_stack.sh
+++ network/delete_stack.sh
@@ -1,1 +1,6 @@
-echo "Delete network"
+STACK_NAME=udagram-network
+REGION=us-east-2
+
+echo "Deleting network stack..."
+aws cloudformation delete-stack --stack-name $STACK_NAME --region=$REGION
+echo "Network stack deleted"
--- network/deploy_stack.sh
+++ network/deploy_stack.sh
@@ -1,1 +1,21 @@
-echo "Deploy network"
+STACK_NAME=udagram-network
+REGION=us-east-2
+
+echo "Deploying network stack..."
+if ! aws cloudformation describe-stacks --region $REGION --stack-name $STACK_NAME ; then
+  echo "Creating network stack..."
+  aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --template-body file://stack.yml \
+  --parameters file://parameters.json \
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+  --region=$REGION
+else
+  echo "Updating network stack..."
+  aws cloudformation update-stack \
+  --stack-name $STACK_NAME \
+  --template-body file://stack.yml \
+  --parameters file://parameters.json \
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+  --region=$REGION
+fi
